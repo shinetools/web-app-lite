41d96d96cd9dd7aa4f8b7e35c4b74d53900d7800
Cédric Kui <cedrickui@gmail.com>
2021-07-23 17:07:59 +0200

    chore(Text): update variant logic for Text color

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,13 +1,25 @@
 import styled from "styled-components";
+
 import { TextProps } from "./types";
 
+const getColorVariant = (variant?: string): number => {
+  switch (variant) {
+    case "lighter":
+      return 1;
+    case "light":
+      return 0;
+    case "secondary":
+      return -1;
+    case "primary":
+    default:
+      return -2;
+  }
+};
+
 const Text = styled.span<TextProps>`
+  color: ${({ variant, theme }) => theme.colors.grey(getColorVariant(variant))};
   font-size: ${({ theme, size = 0 }) => theme.texts.size(size)}px;
   font-weight: ${({ weight = "regular" }) => (weight === "medium" ? 500 : 400)};
-  color: ${({ variant, theme }) =>
-    theme.colors.grey(
-      (variant === "light" && 0) || (variant === "secondary" && -1) || -2
-    )};
 `;
 
 export default Text;
